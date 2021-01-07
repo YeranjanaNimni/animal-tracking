839a530bacc2a6190c09f4ef01c73ebf3675faa8
Yeranjana Nimni <yeranjananimni10@gmail.com>
2021-01-07 23:23:08 +0530

    updated

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.engine('hbs', exphbs({
 app.set("views", path.join(__dirname, "view"));
 app.set('view engine', 'hbs');
 
-const port = new SerialPort("/dev/ttyUSB0", { baudRate: 9600 });
+const port = new SerialPort("COM10", { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
 // http routes
@@ -38,18 +38,17 @@ port.on("open", () => {
   console.log("serial port open");
 });
 
+/*
 // test script
 parser.on('data', (data) => {
   console.log('data :', data);
   io.emit('arduino-data', { data });
-});
+}); */
 
 
-/*
+
 var moduleName;
 var splitData=[];
-var finalValueA=[];
-var finalValueB=[];
 var rssi;
 
 parser.on('data', data =>{
@@ -65,42 +64,50 @@ parser.on('data', data =>{
      
      if(splitData[0]=='+INQ:DCA2:66:44055C'){
         rssi = splitData[2];
-       console.log("this is rssi", rssi)
+      // console.log("this is rssi", rssi)
      } 
   }
   else if(data[2]== 'd'){
 
     if(moduleName=='A'){
+        var finalValueA=[];
         finalValueA.push(rssi);
         console.log("this is final value of A", finalValueA,);
     }
     //console.log("this is rssi value",rssi)
     else if(moduleName=='B'){
+        var finalValueB = [];
         finalValueB.push(rssi);
         console.log("this is final value of B", finalValueB,);
     }   
   }
-} */
 
-/******** Calculate the x,y coodination ***********/
-var a = 5;
-var b = 8;
-var c = 10;
+  var a = 5;
+  var b = 8;
+  var c = 10;
+  
+  var z = 2 * a * c;
+  var cosA = (a * a + c * c - b * b) / z;
+  
+  var A = Math.acos(cosA);
+  var sinA = Math.sin(A);
+  //console.log("this is A", A);
+  
+  var state;
+  var x = a * cosA;
+  var y = a * sinA;
+  
+  if (A < 0 || A > 1) {
+    state = "this is not in the area";
+    console.log(state);
+  } else {
+    state = "the coordinates are";
+   // console.log("this is a x", x); // Menna me x,y values deka fe ekata yawala graph ekak plot karagannai one
+   // console.log("this is a y", y);
+  }
+ io.emit('arduino-data', { state,x, y});
+}) 
 
-var z = 2 * a * c;
-var cosA = (a * a + c * c - b * b) / z;
 
-var A = Math.acos(cosA);
-var sinA = Math.sin(A);
-//console.log("this is A", A);
 
-var x = a * cosA;
-var y = a * sinA;
-
-if (A < 0 || A > 1) {
-  console.log("This is not in the area");
-} else {
-  console.log("this is a x", x); // Menna me x,y values deka fe ekata yawala graph ekak plot karagannai one
-  console.log("this is a y", y);
-}
 
